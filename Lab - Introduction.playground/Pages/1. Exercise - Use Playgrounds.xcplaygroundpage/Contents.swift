aad2ab5499f00ea885926c023a1b89bfb39eed1b
Tom Bast <tbast@maryville.edu>
2017-08-28 16:49:18 -0500

    Changes

--- Lab - Introduction.playground/Pages/1. Exercise - Use Playgrounds.xcplaygroundpage/Contents.swift
+++ Lab - Introduction.playground/Pages/1. Exercise - Use Playgrounds.xcplaygroundpage/Contents.swift
@@ -10,12 +10,14 @@ print("How to use playgrounds to make writing Swift fun and simple")
 /*:
  Now print your own phrases to the console. Pick one of your favorite songs. Use your knowledge of the `print` function to display the song title and artist.
  */
-
+print("Take me out to the ball game")
 
 /*:
  Use multiple `print` functions to write out some of the lyrics to the song.
  */
-
+print("Take me out to the crowd")
+print("Buy me some peanuts and Cracker Jack")
+print("I don't care if I ever get back")
 
 
 /*:
